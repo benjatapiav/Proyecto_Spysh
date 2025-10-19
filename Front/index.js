054a0a2cd9309403accd9998e0fd1ccac0b42dd0
benjamin <benja.tapiav@duocuc.cl>
2025-10-19 20:54:01 -0300

    avance cards spysh

--- Front/index.js
+++ Front/index.js
@@ -1,15 +1,35 @@
+const cards = document.querySelectorAll(".card");
 
-  const cards = document.querySelectorAll('.card');
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
+        entry.target.classList.add("visible");
       }
     });
-  }, {
-    threshold: 0.2  // se activa cuando el 20% de la card es visible
-  });
+  },
+  {
+    threshold: 0.2, // se activa cuando el 20% de la card es visible
+  }
+);
 
-  cards.forEach(card => observer.observe(card));
+cards.forEach((card) => observer.observe(card));
 
+// ================================================================== //
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
